cfc7e61cdeffaf67dafe3936ef405b72fb3082b0
Aannii <anicolmat320@gmail.com>
2022-04-02 11:51:32 -0600

    Actualización del proyecto invernader_CA

--- app/src/main/java/com/example/invernadero_ca/MainActivity.kt
+++ app/src/main/java/com/example/invernadero_ca/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        log.d("Este es mi proyecto de un invernadero")
+        log.d("Este es mi proyecto invernadero")
     }
 }
